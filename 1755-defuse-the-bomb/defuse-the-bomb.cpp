788b430eaeb9e7c6fb629d9afc777a418337ca57
Avinash Kumar <avinashraj9205264621@gmail.com>
2026-05-05 16:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (14.46%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+   vector<int> decrypt(vector<int>& code, int k) {
+    int n = code.size();
+    vector<int> result(n, 0);
+
+    if (k == 0) return result;
+
+    int sum = 0;
+    int start, end;
+
+    // Case 1: k > 0 → forward window
+    if (k > 0) {
+        start = 1;
+        end = k;
+
+        // first window
+        for (int i = start; i <= end; i++) {
+            sum += code[i % n];
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i] = sum;
+
+            // slide window
+            sum -= code[(i + 1) % n];
+            sum += code[(i + k + 1) % n];
+        }
+    }
+
+    // Case 2: k < 0 → backward window
+    else {
+        k = -k;
+        start = n - k;
+        end = n - 1;
+
+        // first window
+        for (int i = start; i <= end; i++) {
+            sum += code[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i] = sum;
+
+            // slide window backward
+            sum -= code[(i - k + n) % n];
+            sum += code[i];
+        }
+    }
+
+    return result;
+}
+};
